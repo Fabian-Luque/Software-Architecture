f46183f312d96ce5cb918ce6c9a76f3580b24d86
fabianluque2014 <fabianluque2014@gmail.com>
2024-06-17 00:00:19 +0000

    implementation of HashTable, ArrayList, StringBuilder

--- 07-Algorithms-and-datastrucures/utils/HashTables/HashTable.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IHashTable<K extends string | number | symbol, V> {
-    put(key: K, value: V): void;
-    get(key: K): V | undefined;
-    remove(key: K): void;
-    containsKey(key: K): boolean;
-    size(): number;
-    clear(): void;
-}
